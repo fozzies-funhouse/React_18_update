1c3f3b1538697e51f823c81a0bed19d96ad03c36
msamori <mahmoud.samori@gmail.com>
2022-07-01 08:40:33 -0400

    updated webpack & webpack-cli; refactored client/index.js from react-dom to react-dom/client as needed for React 18.

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
 import history from './history'
@@ -7,11 +7,16 @@ import store from './store'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
-  <Provider store={store}>
+// react-dom has been replaced with react-dom/client
+// docs:  https://reactjs.org/docs/react-dom-client.html
+// changes that ReactDom.render to createRoot
+// https://stackoverflow.com/questions/71668256/deprecation-notice-reactdom-render-is-no-longer-supported-in-react-18
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(
+<Provider store={store}>
     <Router history={history}>
       <App />
     </Router>
-  </Provider>,
-  document.getElementById('app')
+  </Provider>
 )
