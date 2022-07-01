9beada1f60893d3a63b0cc34fd38560db89597ab
msamori <mahmoud.samori@gmail.com>
2022-07-01 13:21:43 -0400

    added .env file and stashed a secret inside

--- client/store/index.js
+++ client/store/index.js
@@ -9,6 +9,13 @@ import cart from './cart';
 import userProfile from './userProfile';
 import usersReducer from './users';
 
+// redux needs to be refactored or replaced
+// createStore is deprecated
+// thunks are returning promises and not being loaded correctly
+// "We recommend using the configureStore method of the @reduxjs/toolkit package, which replaces createStore."
+// https://redux.js.org/introduction/why-rtk-is-redux-today
+// https://stackoverflow.com/questions/69502147/changing-from-redux-to-redux-toolkit
+
 const reducer = combineReducers({
   auth,
   products,
