6259c3ac8c8d2b22cc164977b403bd428bdaeb76
lkowal25 <leland.kowal@gmail.com>
2022-07-07 18:00:03 -0400

    changed single product to MUI and functional

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,90 +1,152 @@
-// import React, {useEffect, useState}from 'react';
-
-// const SingleProduct = (props) => {
-//   const [ count, setCount] = useState(0);
-//   const { match, getProduct, product, addItemToCart, user, getCart} = props
-
-//   useEffect(()=>{
-//     getProduct(match.params.id);
-//   }, [])
-
-//   return (  );
-// }
-
-// export default SingleProduct;
-
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
 import { connect } from 'react-redux';
 import { fetchProduct, addToCart } from '../store/singleProduct';
 import { fetchCart } from '../store/cart';
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import SingleProduct3D from './SingleProduct/SingleProduct3D';
 
-class SingleProduct extends Component {
-  constructor() {
-    super();
-    this.state = { count: 0 };
-  }
-  async componentDidMount() {
-    this.props.getProduct(this.props.match.params.id);
-    // this.props.getCart(101);
-  }
+import {
+  Container,
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Button,
+} from '@mui/material';
 
-  render() {
-    const { product, addItemToCart, user, getCart } = this.props;
-    return (
-      <Container>
-        <h1
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            color: '#808080',
-            marginBottom: '2rem',
-          }}
-        >
-          Welcome {this.props.user.first_name || 'Guest'}
-        </h1>
-        <h3 style={{ color: '#808080' }}>Product Details</h3>
-        <CardGroup>
-          <Col>
-            <Card
-              className='mb-2'
-              style={{
-                width: '30rem',
-                height: '30rem',
-                color: '#4e4c4b',
-                border: 'none',
-              }}
-            >
-              <SingleProduct3D />
-              {/* <Card.Img variant="top" src={product.image_url} /> */}
-              <Card.Title>{product.name}</Card.Title>
-              <Card.Text>${product.price}</Card.Text>
-              <Card.Text>Inventory: {product.inventory}</Card.Text>
+const SingleProduct = (props) => {
+  const [count, setCount] = useState(0);
+
+  const { getProduct, match, product, addItemToCart, user } = props;
+
+  useEffect(() => {
+    getProduct(match.params.id);
+  }, []);
+
+  return (
+    <Container>
+      <h1
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: '#808080',
+          marginBottom: '2rem',
+        }}
+      >
+        Welcome {user.first_name || 'Guest'}
+      </h1>
+      <h3 style={{ color: '#808080' }}>Product Details</h3>
+      <Grid>
+        <Grid>
+          <Card
+            className="mb-2"
+            style={{
+              width: '30rem',
+              height: '40rem',
+              color: '#4e4c4b',
+              border: 'solid',
+            }}
+          >
+            <SingleProduct3D />
+            {/* <Card.Img variant="top" src={product.image_url} /> */}
+            <CardContent>
+              <CardHeader>{product.name}</CardHeader>
+              <Typography>${product.price}</Typography>
+              <Typography>Inventory: {product.inventory}</Typography>
               <Button
-                className='mt-auto'
-                variant='secondary'
+                className="mt-auto"
+                variant="secondary"
                 onMouseDown={async () => {
-                  await this.props.getProduct(this.props.match.params.id);
+                  await getProduct(match.params.id);
                 }}
                 onMouseUp={async () => {
-                  await addItemToCart(this.props.match.params.id, user.id);
+                  await addItemToCart(match.params.id, user.id);
                 }}
               >
                 Add to Cart
               </Button>
-            </Card>
-          </Col>
-        </CardGroup>
-      </Container>
-    );
-  }
-}
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+// import React, { Component } from 'react';
+
+// import { fetchProduct, addToCart } from '../store/singleProduct';
+// import { fetchCart } from '../store/cart';
+// import Container from 'react-bootstrap/Container';
+// import Card from 'react-bootstrap/Card';
+// import CardGroup from 'react-bootstrap/CardGroup';
+// import Col from 'react-bootstrap/Col';
+// import Button from 'react-bootstrap/Button';
+// import SingleProduct3D from './SingleProduct/SingleProduct3D';
+
+// class SingleProduct extends Component {
+//   constructor() {
+//     super();
+//     this.state = { count: 0 };
+//   }
+//   async componentDidMount() {
+//     this.props.getProduct(this.props.match.params.id);
+//     // this.props.getCart(101);
+//   }
+
+//   render() {
+//     const { product, addItemToCart, user, getCart } = this.props;
+//     return (
+// <Container>
+//   <h1
+//     style={{
+//       display: 'flex',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       color: '#808080',
+//       marginBottom: '2rem',
+//     }}
+//   >
+//     Welcome {this.props.user.first_name || 'Guest'}
+//   </h1>
+//   <h3 style={{ color: '#808080' }}>Product Details</h3>
+//   <CardGroup>
+//     <Col>
+//       <Card
+//         className='mb-2'
+//         style={{
+//           width: '30rem',
+//           height: '30rem',
+//           color: '#4e4c4b',
+//           border: 'none',
+//         }}
+//       >
+//         <SingleProduct3D />
+//         {/* <Card.Img variant="top" src={product.image_url} /> */}
+//         <Card.Title>{product.name}</Card.Title>
+//         <Card.Text>${product.price}</Card.Text>
+//         <Card.Text>Inventory: {product.inventory}</Card.Text>
+//         <Button
+//           className='mt-auto'
+//           variant='secondary'
+//           onMouseDown={async () => {
+//             await this.props.getProduct(this.props.match.params.id);
+//           }}
+//           onMouseUp={async () => {
+//             await addItemToCart(this.props.match.params.id, user.id);
+//           }}
+//         >
+//           Add to Cart
+//         </Button>
+//       </Card>
+//     </Col>
+//   </CardGroup>
+// </Container>
+//     );
+//   }
+// }
 
 const mapStateToProps = (state) => ({
   product: state.product,
