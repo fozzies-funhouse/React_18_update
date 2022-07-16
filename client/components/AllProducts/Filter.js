ad150db7ac4abd8a61a3f33e6bcbda2b9bf98714
Christian Bermeo <christiancabp@gmail.com>
2022-07-16 01:04:18 -0400

    Filter Tags styled and animated with Framer Motion

--- client/components/AllProducts/Filter.js
+++ client/components/AllProducts/Filter.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import './AllProducts.css';
 
 const Filter = ({ tag }) => {
   return (
     <>
-      <h2>{tag}</h2>
+      <motion.div
+        className='filter-item'
+        whileInView={{ scale: [0, 1] }}
+        whileHover={{ scale: [1, 1.2] }}
+        transition={{ duration: 0.25 }}
+      >
+        <img className='filter-image' src={`./Tags/${tag}.png`} alt={tag} />
+      </motion.div>
     </>
   );
 };
 
 export default Filter;
+
+// Nike converse Adidas vans jordans
